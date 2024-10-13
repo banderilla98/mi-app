0d66f5a53f2fcd06f1d31c82e39dbb8039962375
Juan Zahid Angeles Sarabia <zahidangeles11@gmail.com>
2024-10-13 12:01:16 -0700

    Creando el Navbar e importandolo en la raiz principal App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar';
+import './main.css';
+
+
+function App()
+{
+  return (
+    <div>
+      Hola, mundo
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App;
